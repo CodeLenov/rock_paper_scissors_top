bfb622fcf94e922fa64c3c6878b345e95da2e0cc
codelenov <codelenovo@tutanota.com>
2024-04-04 17:04:46 +0200

    WRITE code the game

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+let computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice() {
+
 	const computerChoice = Math.random();
+
 	if (computerChoice < ( 1 / 3) ) {
 		return "rock";
 	} else if (computerChoice > ( 2 / 3) ) {
@@ -7,24 +14,78 @@ function getComputerChoice() {
 	} else {
 		return "paper";
 	}
+
 }
 
-function playRound(playerSelection, computerSelection) {
-	let playerSelectionCaseInsensitive = playerSelection.toLowerCase();
+function getPlayerChoice() {
+
+	const playerSelectionCaseInsensitive = prompt('Enter your choice: "rock", "paper" or "scissors"').toLowerCase();
 	
-	if (playerSelectionCaseInsensitive === computerSelection) {
-		console.log(`You chose the ${playerSelectionCaseInsensitive}\nComputer chose the ${computerSelection}\n\nIT'S A DRAW!`);
-	} else if (
-		(playerSelectionCaseInsensitive === "rock" && computerSelection === "scissors") || 
-		(playerSelectionCaseInsensitive === "paper" && computerSelection === "rock") || 
-		(playerSelectionCaseInsensitive === "scissors" && computerSelection === "paper") 
+	if ( 
+		(playerSelectionCaseInsensitive === "rock") || 
+		(playerSelectionCaseInsensitive === "paper") || 
+		(playerSelectionCaseInsensitive === "scissors")
 		) {
-		console.log(`You chose the ${playerSelectionCaseInsensitive}\nComputer chose the ${computerSelection}\n\nYOU WIN!`);
+		return playerSelectionCaseInsensitive;
 	} else {
-		console.log(`You chose the ${playerSelectionCaseInsensitive}\nComputer chose the ${computerSelection}\n\nCOMPUTER WIN!`);
+		return null;
 	}
+
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playRound() {
+
+	if (playerSelection === null) {
+		console.log(`You entered the undefined word\n\nPlease, RELOAD THE PAGE and\nenter one of these words: "rock", "paper" or "scissors"`);
+	} else if (playerSelection === computerSelection) {
+		console.log(`Computer chose the "${computerSelection}"\nYou chose the "${playerSelection}"\n\nIT'S A DRAW!`);
+	} else if (
+		(playerSelection === "rock" && computerSelection === "scissors") || 
+		(playerSelection === "paper" && computerSelection === "rock") || 
+		(playerSelection === "scissors" && computerSelection === "paper") 
+		) {
+		console.log(`Computer chose the "${computerSelection}"\nYou chose the "${playerSelection}"\n\nYOU WIN!`);
+		return playerScore = ++playerScore;
+	}	else {
+		console.log(`Computer chose the "${computerSelection}"\nYou chose the "${playerSelection}"\n\nCOMPUTER WIN!`);
+		return computerScore = ++computerScore;
+	}
+
+}
+
+function playGame() {
+
+	playRound();
+	console.log(`Computer : You\n\n${computerScore} : ${playerScore}`);
+	
+	computerSelection = getComputerChoice();
+	playerSelection = getPlayerChoice();
+	playRound();
+	console.log(`Computer : You\n\n${computerScore} : ${playerScore}`);
+
+	computerSelection = getComputerChoice();
+	playerSelection = getPlayerChoice();
+	playRound();
+	console.log(`Computer : You\n\n${computerScore} : ${playerScore}`);
+	
+	computerSelection = getComputerChoice();
+	playerSelection = getPlayerChoice();
+	playRound();
+	console.log(`Computer : You\n\n${computerScore} : ${playerScore}`);
+	
+	computerSelection = getComputerChoice();
+	playerSelection = getPlayerChoice();
+	playRound();
+	console.log(`Computer : You\n\n${computerScore} : ${playerScore}`);
+
+	if (computerScore = playerScore) {
+		console.log(`TOTAL SCORE\n\nIT'S A DRAW!\n\nYou can play new game\nReload the page`);
+	} else if (computerScore > playerScore) {
+		console.log(`TOTAL SCORE\n\nCOMPUTER WIN!\n\nYou can play new game\nReload the page`);
+	} else {
+		console.log(`TOTAL SCORE\n\nYOU WIN!\n\nYou can play new game\nReload the page`);
+	}
+	
+}
+
+playGame();
